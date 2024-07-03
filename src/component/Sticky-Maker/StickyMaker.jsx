e36a17bc95a92d44ab3b601bd5fafe38c5e5d265
Ahmed Nady <120312236+NAADDY@users.noreply.github.com>
2024-07-03 04:10:31 +0300

    Fix build issues and ESLint errors

--- src/component/Sticky-Maker/StickyMaker.jsx
+++ src/component/Sticky-Maker/StickyMaker.jsx
@@ -3,18 +3,16 @@ import "./Sticky-Maker.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export default function Sticky(props) {
+const StickyMaker = (props) => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
   const handleTitleInput = (event) => {
     setTitle(event.target.value);
-
   };
 
   const handleBodyInput = (event) => {
     setBody(event.target.value);
-    
   };
 
   const saveNote = () => {
@@ -27,14 +25,35 @@ export default function Sticky(props) {
     <div className='sticky-note-container col-lg-3'>
       <div className="sticky-note-input">
         <div className="sticky-top">
-          <input type="text" className="form-control form-control-md w-50" value={title} onChange={handleTitleInput} />
+          <input 
+            type="text" 
+            className="form-control form-control-md w-50" 
+            value={title} 
+            onChange={handleTitleInput} 
+          />
           <label className="form-label text-bold fw-bold">Title</label>
-          <FontAwesomeIcon icon={faTrash} className='mx-2 icon' onClick={() => props.deleteInputField(props.index)} />
+          <FontAwesomeIcon 
+            icon={faTrash} 
+            className='mx-2 icon' 
+            onClick={() => props.deleteInputField(props.index)} 
+          />
         </div>
         <hr />
-        <input type="text" className="form-control form-control-md w-100" value={body} onChange={handleBodyInput} />
-        <button className="btn btn-outline-dark btn-md px-3 mt-2" onClick={saveNote}>Save</button>
+        <input 
+          type="text" 
+          className="form-control form-control-md w-100" 
+          value={body} 
+          onChange={handleBodyInput} 
+        />
+        <button 
+          className="btn btn-outline-dark btn-md px-3 mt-2" 
+          onClick={saveNote}
+        >
+          Save
+        </button>
       </div>
     </div>
   );
 }
+
+export default StickyMaker;
--- src/component/Sticky/Sticky.jsx
+++ src/component/Sticky/Sticky.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import "./Sticky.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons' 
+import React from 'react';
+import "./Sticky.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export default function Sticky(props) {
-
-  
+const Sticky = (props) => {
   return (
-    <>
-          <div className="container ">
+    <div className="container">
+      <div>
+        <div className="col-md-6">
+          <div className="sticky-note">
+            <div className="sticky-top">
+              <h3>{props.StckyNote.title}</h3>
               <div>
-                  <div className="col-md-6">
-                      <div className="sticky-note">
-                        <div className="sticky-top">
-                          <h3>{props.StckyNote.title}</h3>
-                          <div>
-                          <FontAwesomeIcon icon={faTrash} className='mx-2 icon ' onClick={() => props.deleteSticky(props.index)} />
-                          </div>
-                          </div>
-
-                          <hr />
-                          <p>{props.StckyNote.body}</p>
-                      </div>
-                  </div>
+                <FontAwesomeIcon 
+                  icon={faTrash} 
+                  className='mx-2 icon' 
+                  onClick={() => props.deleteSticky(props.index)} 
+                />
               </div>
+            </div>
+            <hr />
+            <p>{props.StckyNote.body}</p>
           </div>
-
-    </>
-  )
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default Sticky;
